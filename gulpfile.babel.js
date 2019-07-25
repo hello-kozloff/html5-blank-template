497e249f038686078f238b7e6c46f88c975ff86e
hello-kozloff <hello.kozloff@gmail.com>
2019-07-25 21:58:10 +0300

    chore<project>: add task create --block

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const args = require("yargs").argv;
+const fs = require("fs");
+
 import gulp from "gulp";
 import pug from "gulp-pug";
 
@@ -10,4 +13,30 @@ gulp.task("build-pages", function () {
     cache: false
   }))
   .pipe(gulp.dest("static"))
+});
+
+gulp.task("create", function () {
+  const blockName = args.block;
+
+  if ((typeof blockName !== "string") || blockName.length <= 0) {
+    return console.log("[Invalid parameter] Block name is string. Example: create --block example-block");
+  }
+
+  const fileTypes = [
+    "sass",
+    "pug"
+  ];
+
+  fs.mkdirSync(`src/blocks/${blockName}`);
+
+  fileTypes.forEach(file => {
+    fs.writeFile(`src/blocks/${blockName}/${blockName}.${file}`, `.${blockName}`, (error) => {
+      if (error) throw error;
+    });
+  });
+
+  fs.appendFileSync("src/blocks/autoload.sass", `\n@import "${blockName}/${blockName}"`, (error) => {
+    if (error) throw error;
+  });
+
 });
